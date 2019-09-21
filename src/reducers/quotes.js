5f11c37da05992db410fe104932a8d4dedcfbb92
Ashleigh Chun <ashleigh.chun@gmail.com>
2019-09-21 16:02:54 -0700

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,26 @@
+//what is given
+// export default (state = [], action) => {
+//   return state;
+// }
+
+
 export default (state = [], action) => {
-  return state;
+  switch (action.type) {
+    case 'ADD_QUOTE':
+      return state.concat(action.quote)
+    case 'REMOVE_QUOTE':
+      return state.filter(quote => quote.id !== action.quoteId)
+    case 'UPVOTE_QUOTE':
+      return state.map(quote => {
+        if (quote.id === action.quoteId) {
+          debugger
+          quote.votes = quote.votes + 1
+      } else {
+        quote.votes
+      }})
+    case 'DOWNVOTE_QUOTE':
+
+    default:
+      return state;
+  }
 }
